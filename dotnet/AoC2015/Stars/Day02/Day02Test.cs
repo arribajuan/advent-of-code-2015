93eda2afe6327f6b754ac2ea6a688363ef934884
Juan Martinez <jmartinez@flinks.com>
2023-11-15 14:03:19 -0500

    Day 2 - Star 1 - added unit tests Renamed some other unit tests

--- dotnet/AoC2015/Stars/Day02/Day02Test.cs
+++ dotnet/AoC2015/Stars/Day02/Day02Test.cs
@@ -3,4 +3,13 @@ namespace Stars.Day02;
 public class Day02Test
 {
     
+    [Theory]
+    [InlineData(2, 3, 4, 58)]
+    [InlineData(1, 1, 10, 43)]
+    public void Test_Day2_CalculateFinalFloor_SampleDirections(int length, int width, int height, int totalPaper)
+    {
+        var d02 = new Day02();
+        Assert.Equal(totalPaper , d02.CalculateSquareFeet(length, width, height));
+    } 
+    
 }
