09f2332545e826a8598037d0c0386599734e8c91
Juan Martinez <jmartinez@flinks.com>
2023-11-14 20:14:55 -0500

    Day 1 - Added sample unit tests and placeholder inplementation

--- dotnet/AoC2015/Stars/Day01/Day01.cs
+++ dotnet/AoC2015/Stars/Day01/Day01.cs
@@ -2,5 +2,10 @@ namespace Stars.Day01;
 
 public class Day01
 {
-    
+    public int CalculateFinalFloor(string directions)
+    {
+        var currentFloor = 0;
+        
+        return currentFloor;
+    }
 }
--- dotnet/AoC2015/Stars/Day01/Day01Test.cs
+++ dotnet/AoC2015/Stars/Day01/Day01Test.cs
@@ -5,8 +5,27 @@ namespace Stars.Day01;
 public class Day01Test
 {
     [Fact]
-    public void TestTest()
+    public void TestEmptyDirections()
     {
-        Assert.True(true);
+        var d01 = new Day01();
+        var finalFloor = d01.CalculateFinalFloor("");
+        
+        Assert.Equal(0, finalFloor);
+    }
+    
+    [Theory]
+    [InlineData("(())", 0)]
+    [InlineData("()()", 0)]
+    [InlineData("(((", 3)]
+    [InlineData("(()(()(", 3)]
+    [InlineData("))(((((", 3)]
+    [InlineData("())", -1)]
+    [InlineData("))(", -1)]
+    [InlineData(")))", -3)]
+    [InlineData(")())())", -3)]
+    public void TestSampleDirections(string directions, int finalFloor)
+    {
+        var d01 = new Day01();
+        Assert.True(finalFloor == d01.CalculateFinalFloor(directions));
     }
 }
