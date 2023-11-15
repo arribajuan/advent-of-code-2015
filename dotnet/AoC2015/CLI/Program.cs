6bd57faee66e1dc65d49c33bb92b32369b55a988
Juan Martinez <jmartinez@flinks.com>
2023-11-14 21:12:31 -0500

    Day 1 - Star 1 implementation

--- dotnet/AoC2015/CLI/Program.cs
+++ dotnet/AoC2015/CLI/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+var d01 = new Stars.Day01.Day01();
+var finalFloor = d01.CalculateFinalFloorFromDataFile();
 
-Console.WriteLine("Hello, World!");
+    
+Console.WriteLine("Hello there!");
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Day 1");
+Console.WriteLine($"Star 1 - final floor = {finalFloor}");
+Console.WriteLine("-----------------------------");
--- dotnet/AoC2015/Stars/Day01/Day01.cs
+++ dotnet/AoC2015/Stars/Day01/Day01.cs
@@ -1,10 +1,38 @@
+using Helpers;
+
 namespace Stars.Day01;
 
 public class Day01
 {
+    public int CalculateFinalFloorFromDataFile()
+    {
+        var finalFloor = 0;
+        
+        var day01DataFilePath = AppDomain.CurrentDomain.BaseDirectory + "assets/data/day01-data.txt";
+        var fio = new FileIO();
+        var directions = fio.LoadTextFromFile(day01DataFilePath);
+
+        finalFloor = CalculateFinalFloor(directions);
+
+        return finalFloor;
+    }
+    
     public int CalculateFinalFloor(string directions)
     {
         var currentFloor = 0;
+
+        foreach (var c in directions)
+        {
+            switch (c)
+            {
+                case '(':
+                    currentFloor++;
+                    break;
+                case ')':
+                    currentFloor--;
+                    break;
+            }
+        }
         
         return currentFloor;
     }
