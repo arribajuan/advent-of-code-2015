0143868a0f251906fe276770ed6badf1666282a9
Juan Martinez <jmartinez@flinks.com>
2023-11-17 15:07:39 -0500

    Day 3 - passed unit tests

--- dotnet/AoC2015/Stars/Day03/Day03.cs
+++ dotnet/AoC2015/Stars/Day03/Day03.cs
@@ -2,15 +2,53 @@ namespace Stars.Day03;
 
 public class Day03
 {
+
+    
     public DeliveryResults ExecuteMoves(string moves)
     {
+        var xCoordinate = 0;
+        var yCoordinate = 0;
+        var deliveryLog = new Dictionary<string, int>();
+
+        DeliverToCoordinate(xCoordinate, yCoordinate, ref deliveryLog);
+        
+        foreach (var c in moves)
+        {
+            switch (c)
+            {
+                case '<':
+                    xCoordinate--;
+                    break;
+                case '>':
+                    xCoordinate++;
+                    break;
+                case 'v':
+                    yCoordinate--;
+                    break;
+                case '^':
+                    yCoordinate++;
+                    break;
+            }
+            
+            DeliverToCoordinate(xCoordinate, yCoordinate, ref deliveryLog);
+        }
 
         var results = new DeliveryResults()
         {
-            HousesWithAtLeastOnePresent = 0
+            HousesWithAtLeastOnePresent = deliveryLog.Count
         };
         
         return results;
+    }
+    
+    private void DeliverToCoordinate(int xCoordinate, int yCoordinate, ref Dictionary<string, int> deliveryLog)
+    {
+        var coordinateString = xCoordinate + "-" + yCoordinate;
+
+        if (!deliveryLog.TryAdd(coordinateString, 1))
+        {
+            deliveryLog[coordinateString]++;
+        }
     }
 }
 
