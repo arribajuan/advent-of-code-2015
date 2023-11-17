92bb5e839e9f4da0a7d41cbc0efcd741a6b59c1f
Juan Martinez <jmartinez@flinks.com>
2023-11-17 14:43:19 -0500

    Day 3 - Setup

---
+++ dotnet/AoC2015/Stars/Day03/Day03.cs
@@ -0,0 +1,20 @@
+namespace Stars.Day03;
+
+public class Day03
+{
+    public DeliveryResults ExecuteMoves(string moves)
+    {
+
+        var results = new DeliveryResults()
+        {
+            HousesWithAtLeastOnePresent = 0
+        };
+        
+        return results;
+    }
+}
+
+public class DeliveryResults
+{
+    public int HousesWithAtLeastOnePresent = 0;
+}
