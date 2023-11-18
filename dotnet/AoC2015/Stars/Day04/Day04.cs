514c3e2e184b5b336b4c9dca7f727466a8a257cc
Juan Martinez <jmartinez@flinks.com>
2023-11-17 20:56:09 -0500

    Day 4 - setup

---
+++ dotnet/AoC2015/Stars/Day04/Day04.cs
@@ -0,0 +1,11 @@
+namespace Stars.Day04;
+
+public class Day04
+{
+    public int MineAdventCoin(string secretKey)
+    {
+        var lowestPositiveNumber = 0;
+        
+        return lowestPositiveNumber;
+    }
+}
