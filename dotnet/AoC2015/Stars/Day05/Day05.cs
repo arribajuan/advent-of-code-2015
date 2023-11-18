80dcf8534cb5b358fa97898dae9db92b98eef282
Juan Martinez <jmartinez@flinks.com>
2023-11-18 07:36:25 -0500

    Day 5 - passed unit tests

--- dotnet/AoC2015/Stars/Day05/Day05.cs
+++ dotnet/AoC2015/Stars/Day05/Day05.cs
@@ -6,6 +6,39 @@ public class Day05
     {
         var isNice = false;
 
+        var vowelList = new List<char>{ 'a', 'e', 'i', 'o', 'u' };
+        var forbiddenTextList = new List<string> { "ab", "cd", "pq", "xy"};
+        
+        var vowelCount = 0;
+        var repeatedCharacterCount = 0;
+        
+        for (var i = 0; i < stringToTest.Length; i++)
+        {
+            if (vowelList.Contains(stringToTest[i]))
+            {
+                vowelCount++;
+            }
+
+            if (i > 0)
+            {
+                var  twoChars = stringToTest[i - 1] + "" + stringToTest[i];
+                if (forbiddenTextList.Contains(twoChars))
+                {
+                    return false;
+                }
+                
+                if (stringToTest[i] == stringToTest[i - 1])
+                {
+                    repeatedCharacterCount++;
+                }
+            }
+        }
+        
+        if (vowelCount >= 3 && repeatedCharacterCount > 0)
+        {
+            isNice = true;
+        }
+        
         return isNice;
     }
 }
