f70f354f4ab0920552216059b86cb1086ac7d162
Juan Martinez <jmartinez@flinks.com>
2023-11-17 20:37:46 -0500

    Day 3 - refactor for a generic solution

--- dotnet/AoC2015/CLI/Program.cs
+++ dotnet/AoC2015/CLI/Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello there!");
+Console.WriteLine("-----------------------------");
 
 #region Day 1
 
 var d01 = new Stars.Day01.Day01();
 var d01FinalFloor = d01.CalculateFinalFloorFromDataFile();
 var d01IndexReachingBasement = d01.FindIndexWhenReachingBasementFromDataFile();
-    
-Console.WriteLine("-----------------------------");
+
 Console.WriteLine("Day 1");
 Console.WriteLine($" Star 1 - final floor = {d01FinalFloor}");
 Console.WriteLine($" Star 2 - index when reaching basement = {d01IndexReachingBasement}");
@@ -21,7 +21,6 @@ Console.WriteLine("-----------------------------");
 var d02 = new Stars.Day02.Day02();
 var d02PackageInformation = d02.CalculateSquareFeetFromDataFile();
 
-Console.WriteLine("-----------------------------");
 Console.WriteLine("Day 2");
 Console.WriteLine($" Star 1 - total paper = {d02PackageInformation.TotalPaper}");
 Console.WriteLine($" Star 2 - total ribbon = {d02PackageInformation.TotalRibbon}");
@@ -32,13 +31,12 @@ Console.WriteLine("-----------------------------");
 #region Day 3
 
 var d03 = new Stars.Day03.Day03();
-var d03Result1 = d03.ExecuteMovesFromFile();
-var d03Result2 = d03.ExecuteMovesRoboSantaFromFile();
+var d03Result1 = d03.ExecuteMovesFromFile(0);
+var d03Result2 = d03.ExecuteMovesFromFile(1);
 
-Console.WriteLine("-----------------------------");
 Console.WriteLine("Day 3");
-Console.WriteLine($" Star 1 - houses with at least one present = {d03Result1.HousesWithAtLeastOnePresent}");
-Console.WriteLine($" Star 2 - houses with at least one present = {d03Result2.HousesWithAtLeastOnePresent}");
+Console.WriteLine($" Star 1 - houses with at least one present - santa = {d03Result1.HousesWithAtLeastOnePresent}");
+Console.WriteLine($" Star 2 - houses with at least one present - santa + 1 robo santa = {d03Result2.HousesWithAtLeastOnePresent}");
 Console.WriteLine("-----------------------------");
 
 #endregion
